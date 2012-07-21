6f99500314fb01e913f796e4c1970fc355255657
Kenneth Reitz <me@kennethreitz.com>
2012-07-20 22:31:57 -0400

    basics

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yilanbaz:app -b 0.0.0.0:$PORT -w 5 -k gevent -t 3
---
+++ yilanbaz/core.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+from .versions import versions
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
