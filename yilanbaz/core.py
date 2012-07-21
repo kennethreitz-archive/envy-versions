35af08b4a8295abc458ea0bfb598c47d13a68cb7
Kenneth Reitz <me@kennethreitz.com>
2012-07-20 23:14:21 -0400

    requests

--- yilanbaz/core.py
+++ yilanbaz/core.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 
+import requests
 from flask import Flask
 
 from .versions import versions
 
+DIST_BASE 'http://envy-versions.s3.amazonaws.com/'
+
 app = Flask(__name__)
+
+
+def fetch_dist(dist):
+    url = DIST_BASE + dist
+    r = requests.get(url)
+    r.raise_for_status()
+
+    return r.iter_content(1000)
 
 @app.route("/")
 def hello():
     return "Hello World..."
+
+
+@app.reoute('/dists/<dist>')
+def get_dist(dist):
+    return fetch_dist(dist)
+
+
 
 if __name__ == "__main__":
     app.run()
